532d1afe02ebf5d7750a4e97aec20dcaddadec02
mediumthor <liltimefrog@protonmail.com>
2025-06-15 21:44:14 -0500

    hgf

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // ✅ root because it's a standalone app now
   plugins: [react()],
   server: {
     host: '0.0.0.0',
